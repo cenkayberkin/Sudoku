eda577fa328d0b9331472235c60ab887336251dd
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-06-28 20:32:05 +0300

    validation functions added

---
+++ Sudoku/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Sudoku
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+//			int[,] input = new int[9,9] {
+//				{0,0,1,2,0,3,4,0,0},
+//				{0,7,0,0,1,0,0,0,0},
+//				{5,0,0,9,0,6,0,0,0},
+//				{0,0,0,0,0,0,0,0,0},
+//				{6,0,0,0,0,0,0,0,0},
+//				{0,0,0,0,0,0,0,0,0},
+//				{0,0,0,0,0,0,0,0,0},
+//				{9,0,0,0,0,0,0,0,0},
+//				{0,0,0,0,0,0,0,0,0}
+//			};
+
+			int[,] input = new int[9, 9] {
+				{ 1, 1, 1, 2, 1, 3, 4, 1, 1 },
+				{ 1, 1, 1, 2, 1, 3, 4, 1, 1 },
+				{ 1, 1, 1, 2, 1, 3, 4, 1, 1 },
+				{ 1, 1, 1, 2, 1, 3, 4, 1, 1 },
+				{ 1, 1, 1, 2, 1, 3, 4, 1, 1 },
+				{ 1, 1, 1, 2, 1, 3, 4, 0, 1 },
+				{ 1, 1, 1, 2, 1, 3, 4, 1, 1 },
+				{ 1, 1, 1, 2, 1, 3, 4, 1, 1 },
+				{ 1, 1, 1, 2, 1, 3, 4, 1, 1 }
+			};
+
+			SudokuSolver ss = new SudokuSolver (input);
+//			ss.Print (input);
+
+			Console.WriteLine (string.Join(" " , ss.FindCandidates(1,3)));
+			if (ss.FindNextEmptyCell () == null) {
+				Console.WriteLine ("Sudoku finished");
+			} else {
+				Console.WriteLine (ss.FindNextEmptyCell ());
+			}
+
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ Sudoku/SudokuSolver.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sudoku
+{
+	public class SudokuSolver
+	{
+		public int[,] table;
+		public SudokuSolver (int [,]table)
+		{
+			this.table = table;
+		}
+
+		public Tuple<int,int> FindNextEmptyCell()
+		{	
+			for (int row = 0; row < 9; row++) {
+				for (int col = 0; col < 9; col++) {
+					if (table[row,col] == 0) {
+						return new Tuple<int,int> (row, col);
+					}
+				}
+			}
+			return null;
+		}
+
+		public List<int> FindCandidates(int row,int col)
+		{
+			List<int> result = new List<int> ();
+			for (int i = 0; i < 9; i++) {
+				if (IsValid(row,col,i)) {
+					result.Add (i);
+				}
+			}
+			return result;
+		}
+
+		public void Print(int[,] result)
+		{
+			string tmp = "";
+			for (int row = 0; row < 9; row++) {
+				
+				for (int col = 0; col < 9; col++) {
+					tmp += result [row, col] + " ";	
+				}
+				Console.WriteLine (tmp);
+				tmp = "";
+			}
+		}
+
+		public bool IsValid(int row,int col, int value)
+		{
+			if (IsValidByRow (row, value) && IsValidByCol (col, value) && IsValidBySquare (row, col, value)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public bool IsValidByRow(int row, int value)
+		{
+			for (int i = 0; i < 9; i++) {
+				if (table[row,i] == value) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public bool IsValidByCol(int col, int value)
+		{
+			for (int i = 0; i < 9; i++) {
+				if (table[i,col] == value) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public bool IsValidBySquare(int row, int col, int value)
+		{
+			int sectionCol = col / 3;
+			int startCol = sectionCol * 3;
+			int endCol = (sectionCol * 3) + 3;
+
+			int sectionRow = row / 3;
+			int startRow = sectionRow * 3;
+			int endRow = (sectionRow * 3) + 3;
+
+			for (int i = startRow; i < endRow; i++) {
+				for (int z = startCol; z < endCol; z++) {
+					if (table[i,z] == value) {
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+	}
+}
+
