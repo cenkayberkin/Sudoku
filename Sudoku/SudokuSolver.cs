2f1fa153df059bc5119ccb14a6fb6bfb6a4849b1
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-06-28 23:29:22 +0300

    Sudoku Solved

--- Sudoku/SudokuSolver.cs
+++ Sudoku/SudokuSolver.cs
@@ -2,18 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-
+	
 namespace Sudoku
 {
 	public class SudokuSolver
 	{
-		public int[,] table;
+		public int[,] sTable;
 		public SudokuSolver (int [,]table)
 		{
-			this.table = table;
+			this.sTable = table;
+
+			Solve (this.sTable);
+
 		}
 
-		public Tuple<int,int> FindNextEmptyCell()
+		public bool Solve(int[,] table)
+		{
+			if (FindNextEmptyCell(table) == null) {
+				this.sTable = table;
+				return true;
+			}
+
+			Tuple<int,int> t = FindNextEmptyCell (table);
+			int row = t.Item1;
+			int col = t.Item2;
+			var candidates = FindCandidates (row, col, table);
+			foreach (var item in candidates) {
+				table [row, col] = item;
+				if (Solve(table)) {
+					return true;
+				}
+				table [row, col] = 0;
+			}
+			return false;
+		}
+
+		public Tuple<int,int> FindNextEmptyCell(int[,] table)
 		{	
 			for (int row = 0; row < 9; row++) {
 				for (int col = 0; col < 9; col++) {
@@ -25,40 +49,40 @@ namespace Sudoku
 			return null;
 		}
 
-		public List<int> FindCandidates(int row,int col)
+		public List<int> FindCandidates(int row,int col,int[,] table)
 		{
 			List<int> result = new List<int> ();
-			for (int i = 0; i < 9; i++) {
-				if (IsValid(row,col,i)) {
+			for (int i = 1; i < 10; i++) {
+				if (IsValid(row,col,i,table)) {
 					result.Add (i);
 				}
 			}
 			return result;
 		}
 
-		public void Print(int[,] result)
+		public void Print()
 		{
 			string tmp = "";
 			for (int row = 0; row < 9; row++) {
 				
 				for (int col = 0; col < 9; col++) {
-					tmp += result [row, col] + " ";	
+					tmp += this.sTable [row, col] + " ";	
 				}
 				Console.WriteLine (tmp);
 				tmp = "";
 			}
 		}
 
-		public bool IsValid(int row,int col, int value)
+		public bool IsValid(int row,int col, int value,int[,] table)
 		{
-			if (IsValidByRow (row, value) && IsValidByCol (col, value) && IsValidBySquare (row, col, value)) {
+			if (IsValidByRow (row, value,table) && IsValidByCol (col, value,table) && IsValidBySquare (row, col, value,table)) {
 				return true;
 			} else {
 				return false;
 			}
 		}
 
-		public bool IsValidByRow(int row, int value)
+		public bool IsValidByRow(int row, int value,int[,] table)
 		{
 			for (int i = 0; i < 9; i++) {
 				if (table[row,i] == value) {
@@ -68,7 +92,7 @@ namespace Sudoku
 			return true;
 		}
 
-		public bool IsValidByCol(int col, int value)
+		public bool IsValidByCol(int col, int value, int[,] table)
 		{
 			for (int i = 0; i < 9; i++) {
 				if (table[i,col] == value) {
@@ -78,7 +102,7 @@ namespace Sudoku
 			return true;
 		}
 
-		public bool IsValidBySquare(int row, int col, int value)
+		public bool IsValidBySquare(int row, int col, int value, int[,] table)
 		{
 			int sectionCol = col / 3;
 			int startCol = sectionCol * 3;
